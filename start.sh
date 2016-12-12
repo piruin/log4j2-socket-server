8b37afaef327593f2f250298c17cbcbdae5313d0
Piruin Panichphol <piruin.p@gmail.com>
2016-12-12 17:00:48 +0700

    Relocate .jar

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 echo "Start log4j 2 Socket Server..."
 
-java -cp jcommander-1.58.jar;log4j-api-2.7.jar;log4j-core-2.7.jar org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j.xml
+java -cp libs/* org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j.xml
 
 echo "Stop server."
