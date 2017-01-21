ff3b211c7b7e3defe901ea645e2dba9020e4c2ec
Piruin Panichphol <piruin.p@gmail.com>
2017-01-21 12:54:42 +0700

    fix command error on unix

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 echo "Start log4j 2 Socket Server..."
 
-java -cp libs/* org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j.xml
+java -server -cp "libs/*" org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j.xml
 
 echo "Stop server."
