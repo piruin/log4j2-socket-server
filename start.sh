633e72fdc657324f9a57c97c259430310c706e72
Piruin Panichphol <piruin.p@gmail.com>
2017-01-22 14:01:43 +0700

    rename configuration file

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 echo "Start log4j 2 Socket Server..."
 
-java -server -cp "libs/*" org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j.xml
+java -server -cp "libs/*" org.apache.logging.log4j.core.net.server.TcpSocketServer -i -p 8000 -c log4j2.xml
 
 echo "Stop server."
